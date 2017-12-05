7027b4f734873ca9ea04fce0a176375149d0c05c
Johan Swanepoel <johanswanepoel@Johans-MacBook-Pro-2.local>
2017-12-05 20:16:42 +1100

    Day 1, Day 2

--- day2.py
+++ day2.py
@@ -1,27 +1,25 @@
 import sys
+import csv
 
 def day2():
-    w, h = 4, 3;
+    w, h = 16, 16;
     matrix = [[None for x in range(w)] for y in range(h)]
-    matrix[0][0] = 5
-    matrix[0][1] = 1
-    matrix[0][2] = 9
-    matrix[0][3] = 5
 
-    matrix[1][0] = 7
-    matrix[1][1] = 5
-    matrix[1][2] = 3
-    matrix[1][3] = None
-
-    matrix[2][0] = 2
-    matrix[2][1] = 4
-    matrix[2][2] = 6
-    matrix[2][3] = 8
-
+    datafile = open('day2.csv', 'r')
+    datareader = csv.reader(datafile, delimiter=',')
+    matrix = []
+    tmprow = []
+    for row in datareader:
+        print("ROW",row)
+        tmprow=[]
+        for v in row:
+            tmprow.append(int(v))
+        matrix.append(tmprow)
+        print(matrix)
     calc(matrix)
 
 def calc( matrix ):
-    print ("Matrix: ", matrix)
+    # print ("Matrix: ", matrix)
     checksum = 0
     minv = 0
     maxv = 0
@@ -29,6 +27,9 @@ def calc( matrix ):
         print(row)
         minv = min(v for v in row if v is not None)
         maxv = max(v for v in row if v is not None)
+        print("Min",minv)
+        print("Max",maxv)
+        print("Dff",(maxv-minv))
         checksum = checksum + (maxv - minv)
     print("Checksum",checksum)
 
